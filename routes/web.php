880258e2e78a3612d91b916dde48cc39c62dc823
Sofia Carballo <sofia.carballo@live.com>
2020-05-25 20:42:39 +0200

    Add Product Web Route

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/category', 'CategoryController@index')->name('category.index');
 
+Route::get('/product', 'ProductController@index')->name('product.index');
 
